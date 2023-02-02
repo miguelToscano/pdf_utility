e05d932e2f09499ac9332c40ed6016738d4a4158
miguelToscano <miguetoscano288@gmail.com>
2023-02-02 13:22:40 -0300

    added gepdf

--- pdf/src/application/canister_methods.rs
+++ pdf/src/application/canister_methods.rs
@@ -4,12 +4,51 @@ use ic_kit::ic;
 use ic_kit::macros::*;
 use ic_kit::*;
 use serde::Deserialize;
+// use genpdf;
+// use printpdf;
+
+// pub const LIBERATION_SANS_REGULAR: &[u8] = include_bytes!("../../../fonts/LiberationSans/LiberationSans-Regular.ttf");
+// pub const LIBERATION_SANS_BOLD: &[u8] = include_bytes!("../../../fonts/LiberationSans/LiberationSans-Bold.ttf");
+// pub const LIBERATION_SANS_ITALIC: &[u8] = include_bytes!("../../../fonts/LiberationSans/LiberationSans-Italic.ttf");
+// pub const LIBERATION_SANS_BOLD_ITALIC: &[u8] = include_bytes!("../../../fonts/LiberationSans/LiberationSans-BoldItalic.ttf");
 
 #[query]
 #[candid_method(query)]
 pub fn name() -> String {
     String::from("W3NS")
 }
+
+// pub fn generate_font_family() -> Result<genpdf::fonts::FontFamily<genpdf::fonts::FontData>, genpdf::error::Error> {
+//     Ok(genpdf::fonts::FontFamily {
+//         regular: genpdf::fonts::FontData::new(LIBERATION_SANS_REGULAR.to_vec(), None)?,
+//         bold: genpdf::fonts::FontData::new(LIBERATION_SANS_BOLD.to_vec(), None)?,
+//         italic: genpdf::fonts::FontData::new(LIBERATION_SANS_ITALIC.to_vec(), None)?,
+//         bold_italic: genpdf::fonts::FontData::new(LIBERATION_SANS_BOLD_ITALIC.to_vec(), None)?,
+//     })
+// }
+
+// #[update]
+// #[candid_method(update)]
+// pub async fn generate_pdf() -> Result<(), ()> { 
+//     // Load a font from the file system
+//     // let font_family = genpdf::fonts::from_files("../../fonts", "LiberationSans", None)
+//     // .expect("Failed to load font family");
+//     let font_family = generate_font_family().expect("Failed to load font family");
+//     // genpdf::fonts::FontData::new(LIBERATION_SANS_FONT, None);
+//     // Create a document and set the default font family
+//     let mut doc = genpdf::Document::new(font_family);
+//     // Change the default settings
+//     doc.set_title("Demo document");
+//     // Customize the pages
+//     let mut decorator = genpdf::SimplePageDecorator::new();
+//     decorator.set_margins(10);
+//     doc.set_page_decorator(decorator);
+//     // Add one or more elements
+//     doc.push(genpdf::elements::Paragraph::new("This is a demo document."));
+//     // Render the document and write it to a file
+//     doc.render_to_file("output.pdf").expect("Failed to write PDF file");
+//     Ok(())
+// }
 
 #[query(name = "__get_candid_interface_tmp_hack")]
 fn export_candid() -> String {
